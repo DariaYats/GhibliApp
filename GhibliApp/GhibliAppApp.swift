d9ec31c63ef118ae7b27bc3889a24709deadd0ee
Daria Yats <dariayatsyniuk@gmail.com>
2025-11-11 11:32:37 +0100

    Initial Commit

---
+++ GhibliApp/GhibliAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliAppApp.swift
+//  GhibliApp
+//
+//  Created by Daria Yatsyniuk on 11.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct GhibliAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GhibliAppTests/GhibliAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliAppTests.swift
+//  GhibliAppTests
+//
+//  Created by Daria Yatsyniuk on 11.11.2025.
+//
+
+import Testing
+@testable import GhibliApp
+
+struct GhibliAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
